e91e7920532eca0dcfcd8260b43b7dbddbd36712
Akash Bhadana <23536akash.2021@gmail.com>
2023-08-27 12:23:54 +0530

    router-dom bug fix and header active add on path

---
+++ src/components/NavBar.js
@@ -0,0 +1,40 @@
+import React, { useEffect } from 'react'
+import { Link, useLocation } from "react-router-dom";
+
+const NavBar = () => {
+
+    let location = useLocation();
+    useEffect(() => {
+        console.log(location.pathname);
+    }, [location]);
+
+    return (
+        <div>
+            <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+                <div className="container-fluid">
+                   
+                    <Link className="navbar-brand" to="/">NewsBird </Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item"><Link className={`nav-link ${location.pathname === "/general" ? "active" : ""}`} to="/general">General</Link></li>
+                            <li className="nav-item"><Link className={`nav-link ${location.pathname === "/business" ? "active" : ""}`} to="/business">Business</Link></li>
+                            <li className="nav-item"><Link className={`nav-link ${location.pathname === "/entertainment" ? "active" : ""}`} to="/entertainment">Entertainment</Link></li>
+                            <li className="nav-item"><Link className={`nav-link ${location.pathname === "/health" ? "active" : ""}`} to="/health">Health</Link></li>
+                            <li className="nav-item"><Link className={`nav-link ${location.pathname === "/science" ? "active" : ""}`} to="/science">Science</Link></li>
+                            <li className="nav-item"><Link className={`nav-link ${location.pathname === "/sports" ? "active" : ""}`} to="/sports">Sports</Link></li>
+                            <li className="nav-item"><Link className={`nav-link ${location.pathname === "/technology" ? "active" : ""}`} to="/technology">Technology</Link></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+
+}
+
+export default NavBar
+
+
