bb71cec7ee86da19168c685a747c03424b0e1eee
Akash Bhadana <23536akash.2021@gmail.com>
2023-09-05 11:58:32 +0530

    NavBar  bird icon added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,22 +3,22 @@ import { Link, useLocation } from "react-router-dom";
 
 const NavBar = () => {
     let headingcolor = {
-        color:'	#06b6d4',
-        fontWeight:'500',
-        fontSize: '22px'}
-
+        color: '#06b6d4',
+        fontWeight: '500',
+        fontSize: '22px'
+    }
     let location = useLocation();
-    useEffect(() => {
-        console.log(location.pathname);
-    }, [location]);
-
+    useEffect(() => {console.log(location.pathname); }, [location]);
     return (
         <div>
             <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
                 <div className="container-fluid">
-                   
-                    <Link className="navbar-brand" style={headingcolor} to="/">NewsBird </Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <Link className="navbar-brand" style={headingcolor} to="/">NewsBird <iconify-icon icon="game-icons:hummingbird"></iconify-icon></Link>
+                    <button className="navbar-toggler"
+                     type="button" data-bs-toggle="collapse"
+                      data-bs-target="#navbarSupportedContent" 
+                      aria-controls="navbarSupportedContent"
+                       aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
@@ -36,9 +36,7 @@ const NavBar = () => {
             </nav>
         </div>
     )
-
 }
-
 export default NavBar
 
 
